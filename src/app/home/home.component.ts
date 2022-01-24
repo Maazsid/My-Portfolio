91c88bf4cd08e68a4f5050d05294967f714afdf0
Maaz <maaz.d.sid@gmail.com>
2022-01-25 02:04:46 +0500

    completed projects section and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('projects') projects: ElementRef<HTMLElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  goToProjects() {
+    this.projects.nativeElement.scrollIntoView({behavior: 'smooth'})
   }
-
 }
